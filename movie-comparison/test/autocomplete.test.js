4ae4b53029f58649871398547f3a1d1adf202d23
Svetoslav Krastev <svetoslav.tkrastev@gmail.com>
2020-04-09 05:14:47 +0300

    added a test for autocomplete's dropdown

--- movie-comparison/test/autocomplete.test.js
+++ movie-comparison/test/autocomplete.test.js
@@ -1,11 +1,29 @@
-it('Shows an autocomplete', () => {
+const waitFor = (selector) => {
+  return new Promise((resolve, reject) => {
+    const interval = setInterval(() => {
+      if (document.querySelector(selector)) {
+        clearInterval(interval)
+        clearTimeout(timeout)
+        resolve()
+      }
+    }, 30)
+
+    const timeout = setTimeout(() => {
+      clearInterval(interval)
+      reject()
+    }, 2000)
+  })
+}
+
+beforeEach(() => {
+  document.querySelector('#target').innerHTML = ''
   createAutoComplete({
     root: document.querySelector('#target'),
     fetchData() {
       return [
-        {Title: 'A movie'},
-        {Title: 'Some movie'},
-        {Title: 'Some other movie'},
+        { Title: 'A movie' },
+        { Title: 'Some movie' },
+        { Title: 'Some other movie' },
       ]
     },
     renderOption(movie) {
@@ -13,7 +31,23 @@ it('Shows an autocomplete', () => {
     },
   })
 
+
+})
+
+it('Dropdown starts closed', () => {
   const dropdown = document.querySelector('.dropdown')
 
-  assert.notInclude(dropdown.className, 'is-active')  
+  assert.notInclude(dropdown.className, 'is-active')
+})
+
+it('After searching, dropdown opens up', async () => {
+  const input = document.querySelector('input')
+  input.value = 'inception'
+  input.dispatchEvent(new Event('input'))
+
+  await waitFor('.dropdown-item')
+
+  const dropdown = document.querySelector('.dropdown')
+
+  assert.include(dropdown.className, 'is-active')
 })
